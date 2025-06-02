73b7075caa049f3aa0c5ab327cf173a6fce11bd6
Aleff Souza <aleffjonathan7@gmail.com>
2025-06-02 10:35:16 -0300

    corrigindo verficacao mapeamento

--- app.py
+++ app.py
@@ -7,6 +7,47 @@ import seaborn as sns
 import re
 import numpy as np
 
+# Função para ajustar um polinômio à curva de potência e prever valores de potência esperados
+def fit_polynomial_curve(filtered_data, degree=3):
+    """ Ajusta uma curva polinomial para a relação entre velocidade do vento e potência gerada """
+        
+    # Ajustando um modelo de regressão polinomial para os dados
+    X = filtered_data[col_vel_vento].values.reshape(-1, 1)
+    y = filtered_data[col_pot].values
+        
+    poly = PolynomialFeatures(degree=degree)
+    X_poly = poly.fit_transform(X)
+        
+    model = LinearRegression()
+    model.fit(X_poly, y)
+        
+    # Prevendo a potência com base no modelo ajustado
+    y_pred = model.predict(X_poly)
+        
+    return model, X, y, y_pred
+
+# Função para filtrar dados baseado na previsão da curva de potência ajustada
+def filter_data_based_on_curve(filtered_data, model, X, y_pred, tolerance=50):
+        """ Filtra os dados de forma suave, mantendo apenas os pontos próximos à curva ajustada """
+        
+        # Definir uma margem de erro (tolerância) em torno da curva de potência
+        valid_data = []
+        
+        for i, wind_speed in enumerate(X.flatten()):
+            actual_power = filtered_data.iloc[i][col_pot]
+            predicted_power = y_pred[i]
+            
+            # Se a potência real estiver dentro da tolerância da potência prevista pela curva
+            if abs(actual_power - predicted_power) <= tolerance:
+                valid_data.append(filtered_data.iloc[i])
+        
+        # Criar um DataFrame com os dados válidos
+        valid_df = pd.DataFrame(valid_data)
+        
+        return valid_df
+
+    
+
 def print_curva_potencia(df):
     # Filtrar os dados para garantir que estamos considerando apenas as potências maiores que zero
     filtered_data_power = df[df[col_pot] > 0]
@@ -174,7 +215,6 @@ def met_pz(filtered_data):
     st.subheader("Vértices das parábolas")
     vertices_df = pd.DataFrame(vertices, columns=["Velocidade", "Vértice X", "Vértice Y", "R²"])
     st.dataframe(vertices_df)
-    vertices_df.to_csv("vertices.csv", index=False)
 
     return vertices
 
@@ -223,159 +263,130 @@ if uploaded_file is not None:
     st.markdown("### Mapeamento final:")
     st.write(mapeamento_colunas)
 
-    col_ts = mapeamento_colunas["timestamp"]
-    col_status = mapeamento_colunas["status"]
-    col_pot = mapeamento_colunas["potencia"]
-    col_dir_vento = mapeamento_colunas["direcao_vento"]
-    col_vel_vento = mapeamento_colunas["velocidade_vento"]
-    col_yaw = mapeamento_colunas["angulo_yaw"]
+    if all(mapeamento_colunas.values()) and len(set(mapeamento_colunas.values())) == len(mapeamento_colunas):
+        col_ts = mapeamento_colunas["timestamp"]
+        col_status = mapeamento_colunas["status"]
+        col_pot = mapeamento_colunas["potencia"]
+        col_dir_vento = mapeamento_colunas["direcao_vento"]
+        col_vel_vento = mapeamento_colunas["velocidade_vento"]
+        col_yaw = mapeamento_colunas["angulo_yaw"]
+
+        df = df[list(mapeamento_colunas.values())]
+        df[col_ts] = pd.to_datetime(df[col_ts], errors='coerce')
+
+        valor_ok = st.number_input("Informe o valor considerado OK para STATUS:", value=None)
+
+        filter_1 = df[col_status] == valor_ok
+        filter_3 = df[col_pot] > 300
+        filter_4 = df[col_dir_vento].between(-15, 15)
+        filter_5 = df[col_vel_vento].between(4, 10)
+
+        filtered_data = df[filter_1 & filter_3 & filter_4 & filter_5]
+
+        # Criar intervalos
+        intervalos_velocidade = pd.interval_range(start=4, end=10, freq=0.5)
+        intervalos_direcao = pd.interval_range(start=-12, end=12, freq=1)
+        filtered_data["WNAC_AneWdSpd_intervalo"] = pd.cut(filtered_data[col_vel_vento], bins=intervalos_velocidade)
+        filtered_data["WTUR_WindDir_intervalo"] = pd.cut(filtered_data[col_dir_vento], bins=intervalos_direcao)
+
+        st.subheader("Dados filtrados")
+        st.write(filtered_data.describe())
+
+        # Gráfico de dispersão
+        st.subheader("Gráfico de Dispersão: Velocidade vs Direção do Vento")
+        plt.figure(figsize=(8, 5))
+        plt.scatter(filtered_data[col_vel_vento], filtered_data[col_dir_vento], color='b', alpha=0.5)
+        plt.xlabel("Velocidade do Vento (m/s)")
+        plt.ylabel("Direção do Vento (°)")
+        plt.title("Gráfico de Dispersão: Velocidade vs Direção do Vento")
+        plt.grid(True)
+        st.pyplot(plt.gcf())
+        plt.clf()
+
+        # Novo gráfico de dispersão
+        st.subheader("Dispersão: Velocidade do Vento vs Potência")
+        power_data = filtered_data[filtered_data[col_pot] > 0]
+        plt.figure(figsize=(10, 6))
+        plt.scatter(power_data[col_vel_vento], power_data[col_pot], color='b', alpha=0.5, s=10)
+        plt.title("Dispersão: Velocidade do Vento vs Potência Gerada")
+        plt.xlabel("Velocidade do Vento (m/s)")
+        plt.ylabel("Potência Gerada (kW)")
+        plt.xlim(0, 20)
+        plt.grid(True)
+        st.pyplot(plt.gcf())
+        plt.clf()
+
+        # Calcular a média de potência para cada intervalo de velocidade do vento (WNAC_AneWdSpd_intervalo_SPIN)
+        mean_potencia_wind_speed_interval = filtered_data.groupby("WTUR_WindDir_intervalo")[col_pot].mean()
+
+        # Calcular a média geral de potência
+        mean_potencia_total = filtered_data[col_pot].mean()
+
+        if valor_ok is not None:
+            st.subheader("Limpeza dos dados:")
+            # Suponha que seu DataFrame se chame "filtered_data"
+            with st.spinner("Filtrando dados e gerando gráfico..."):
+
+                model, X, y, y_pred = fit_polynomial_curve(filtered_data, degree=3)
+                filtered_data_cleaned = filter_data_based_on_curve(filtered_data, model, X, y_pred, tolerance=200)
+
+                # Visualizar os dados limpos
+                fig, ax = plt.subplots(figsize=(10, 6))
+                ax.scatter(filtered_data_cleaned[col_vel_vento], filtered_data_cleaned[col_pot], color="red", alpha=0.6, label='Dados Filtrados')
+                # ax.scatter(filtered_data_cleaned['YA_corr_avg'], filtered_data_cleaned[col_pot], color="blue", alpha=0.6, label='Dados Filtrados')
+                # ax.plot(X, y_pred, color='blue', label='Curva de Potência Ajustada (Polinomial)')
+                ax.set_title("Dados Filtrados: Potência vs Velocidade do Vento")
+                ax.set_xlabel("Velocidade do Vento (m/s)")
+                ax.set_ylabel("Potência Gerada (kW)")
+                ax.set_xlim(0, 20)
+                ax.set_ylim(0, 3500)
+                ax.grid(True)
+                ax.legend()
+                st.pyplot(fig)
+
+                # Mostrar os dados limpos
+                st.subheader("Amostra dos Dados Filtrados")
+                st.dataframe(filtered_data_cleaned.describe())
+
+                # Mostrar quantidade de linhas
+                st.write("Número de linhas:", len(filtered_data_cleaned))
+
+            filtered_data = filtered_data_cleaned
+            filtered_data['semana'] = filtered_data[col_ts].dt.to_period('M').apply(lambda r: r.start_time)
+            #df['semana'] = df['TS'].dt.to_period('Q').apply(lambda r: r.start_time)
+            dfs= []
+            dfs.append(filtered_data)
+
+            for i, df in enumerate(dfs):
+                diferencas = []
+                vertices_n = []
+                teste_ver_n = []
+                agrupamento_mensal = df.groupby('semana')
+                lista_de_grupos = list(agrupamento_mensal)
+
+                # Cria uma coluna de rótulo vazia no DataFrame
+                df['rótulo'] = np.nan
+
+                for i in range(len(lista_de_grupos) - 1):
+                    mes_atual, grupo = lista_de_grupos[i]
+
+                    #grupo = grupo[grupo['WTUR_WindDir'] - grupo['YA_corr_avg'] > 0.5]
+                    st.write("### Período ", i+1)
+                    st.write("Data:", mes_atual)
+                    print('######### CATAVENTO ############')
+                    print(f'Semana {mes_atual}')
+                    print_curva_potencia(grupo)
+                    #my = dif_sensor(grupo)
+                    met_pz(grupo)
+    else:
+        st.warning("Por favor, selecione todas as colunas corretamente e sem repetições.")
+
+    
+
+    
 
 
-    df = df[list(mapeamento_colunas.values())]
-    df[col_ts] = pd.to_datetime(df[col_ts], errors='coerce')
-
-    valor_ok = st.number_input("Informe o valor considerado OK para STATUS:", value=None)
-
-    filter_1 = df[col_status] == valor_ok
-    filter_3 = df[col_pot] > 300
-    filter_4 = df[col_dir_vento].between(-15, 15)
-    filter_5 = df[col_vel_vento].between(4, 10)
-
-    filtered_data = df[filter_1 & filter_3 & filter_4 & filter_5]
-
-    # Criar intervalos
-    intervalos_velocidade = pd.interval_range(start=4, end=10, freq=0.5)
-    intervalos_direcao = pd.interval_range(start=-12, end=12, freq=1)
-    filtered_data["WNAC_AneWdSpd_intervalo"] = pd.cut(filtered_data[col_vel_vento], bins=intervalos_velocidade)
-    filtered_data["WTUR_WindDir_intervalo"] = pd.cut(filtered_data[col_dir_vento], bins=intervalos_direcao)
+    
 
 
-    st.subheader("Dados filtrados")
-    st.write(filtered_data.describe())
-
-    # Gráfico de dispersão
-    st.subheader("Gráfico de Dispersão: Velocidade vs Direção do Vento")
-    plt.figure(figsize=(8, 5))
-    plt.scatter(filtered_data[col_vel_vento], filtered_data[col_dir_vento], color='b', alpha=0.5)
-    plt.xlabel("Velocidade do Vento (m/s)")
-    plt.ylabel("Direção do Vento (°)")
-    plt.title("Gráfico de Dispersão: Velocidade vs Direção do Vento")
-    plt.grid(True)
-    st.pyplot(plt.gcf())
-    plt.clf()
-
-    # Novo gráfico de dispersão
-    st.subheader("Dispersão: Velocidade do Vento vs Potência")
-    power_data = filtered_data[filtered_data[col_pot] > 0]
-    plt.figure(figsize=(10, 6))
-    plt.scatter(power_data[col_vel_vento], power_data[col_pot], color='b', alpha=0.5, s=10)
-    plt.title("Dispersão: Velocidade do Vento vs Potência Gerada")
-    plt.xlabel("Velocidade do Vento (m/s)")
-    plt.ylabel("Potência Gerada (kW)")
-    plt.xlim(0, 20)
-    plt.grid(True)
-    st.pyplot(plt.gcf())
-    plt.clf()
-
-    # Calcular a média de potência para cada intervalo de velocidade do vento (WNAC_AneWdSpd_intervalo_SPIN)
-    mean_potencia_wind_speed_interval = filtered_data.groupby("WTUR_WindDir_intervalo")[col_pot].mean()
-
-    # Calcular a média geral de potência
-    mean_potencia_total = filtered_data[col_pot].mean()
-
-
-    # Função para ajustar um polinômio à curva de potência e prever valores de potência esperados
-    def fit_polynomial_curve(filtered_data, degree=3):
-        """ Ajusta uma curva polinomial para a relação entre velocidade do vento e potência gerada """
-        
-        # Ajustando um modelo de regressão polinomial para os dados
-        X = filtered_data[col_vel_vento].values.reshape(-1, 1)
-        y = filtered_data[col_pot].values
-        
-        poly = PolynomialFeatures(degree=degree)
-        X_poly = poly.fit_transform(X)
-        
-        model = LinearRegression()
-        model.fit(X_poly, y)
-        
-        # Prevendo a potência com base no modelo ajustado
-        y_pred = model.predict(X_poly)
-        
-        return model, X, y, y_pred
-
-    # Função para filtrar dados baseado na previsão da curva de potência ajustada
-    def filter_data_based_on_curve(filtered_data, model, X, y_pred, tolerance=50):
-        """ Filtra os dados de forma suave, mantendo apenas os pontos próximos à curva ajustada """
-        
-        # Definir uma margem de erro (tolerância) em torno da curva de potência
-        valid_data = []
-        
-        for i, wind_speed in enumerate(X.flatten()):
-            actual_power = filtered_data.iloc[i][col_pot]
-            predicted_power = y_pred[i]
-            
-            # Se a potência real estiver dentro da tolerância da potência prevista pela curva
-            if abs(actual_power - predicted_power) <= tolerance:
-                valid_data.append(filtered_data.iloc[i])
-        
-        # Criar um DataFrame com os dados válidos
-        valid_df = pd.DataFrame(valid_data)
-        
-        return valid_df
-    print(valor_ok)
-    if valor_ok is not None:
-        st.subheader("Limpeza dos dados:")
-        # Suponha que seu DataFrame se chame "filtered_data"
-        with st.spinner("Filtrando dados e gerando gráfico..."):
-
-            model, X, y, y_pred = fit_polynomial_curve(filtered_data, degree=3)
-            filtered_data_cleaned = filter_data_based_on_curve(filtered_data, model, X, y_pred, tolerance=200)
-
-            # Visualizar os dados limpos
-            fig, ax = plt.subplots(figsize=(10, 6))
-            ax.scatter(filtered_data_cleaned[col_vel_vento], filtered_data_cleaned[col_pot], color="red", alpha=0.6, label='Dados Filtrados')
-            # ax.scatter(filtered_data_cleaned['YA_corr_avg'], filtered_data_cleaned[col_pot], color="blue", alpha=0.6, label='Dados Filtrados')
-            # ax.plot(X, y_pred, color='blue', label='Curva de Potência Ajustada (Polinomial)')
-            ax.set_title("Dados Filtrados: Potência vs Velocidade do Vento")
-            ax.set_xlabel("Velocidade do Vento (m/s)")
-            ax.set_ylabel("Potência Gerada (kW)")
-            ax.set_xlim(0, 20)
-            ax.set_ylim(0, 3500)
-            ax.grid(True)
-            ax.legend()
-            st.pyplot(fig)
-
-            # Mostrar os dados limpos
-            st.subheader("Amostra dos Dados Filtrados")
-            st.dataframe(filtered_data_cleaned.describe())
-
-            # Mostrar quantidade de linhas
-            st.write("Número de linhas:", len(filtered_data_cleaned))
-
-        filtered_data = filtered_data_cleaned
-        filtered_data['semana'] = filtered_data[col_ts].dt.to_period('M').apply(lambda r: r.start_time)
-        #df['semana'] = df['TS'].dt.to_period('Q').apply(lambda r: r.start_time)
-        dfs= []
-        dfs.append(filtered_data)
-
-        for i, df in enumerate(dfs):
-            diferencas = []
-            vertices_n = []
-            teste_ver_n = []
-            agrupamento_mensal = df.groupby('semana')
-            lista_de_grupos = list(agrupamento_mensal)
-
-            # Cria uma coluna de rótulo vazia no DataFrame
-            df['rótulo'] = np.nan
-
-            for i in range(len(lista_de_grupos) - 1):
-                mes_atual, grupo = lista_de_grupos[i]
-
-                #grupo = grupo[grupo['WTUR_WindDir'] - grupo['YA_corr_avg'] > 0.5]
-                st.write("### Período ", i+1)
-                st.write("Data:", mes_atual)
-                print('######### CATAVENTO ############')
-                print(f'Semana {mes_atual}')
-                print_curva_potencia(grupo)
-                #my = dif_sensor(grupo)
-                met_pz(grupo)
+    
